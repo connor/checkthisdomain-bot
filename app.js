46b9cbcfdcad6389f36ba3f9886ef5d22485c4a1
Connor Montgomery <c@cnnr.me>
2012-04-10 18:30:38 -0500

    let's try this out

--- app.js
+++ app.js
@@ -32,47 +32,55 @@ var twit = new Twitter({
     })
     .stream('statuses/filter', {'track':'@checkthisdomain'}, function(stream) {
       stream.on('data', function (tweet) {
+
         if (tweet.in_reply_to_screen_name === "checkthisdomain" && !tweet.retweeted) {
 
-          var shortened_url     = tweet.text.split(' ')[1]
+          var shortened_url     = encodeURIComponent( tweet.text.split(' ')[1] )
             , userToRespondTo   = tweet.user.screen_name
-            , expanded_url
+            , expanded_url;
 
 
-          // TODO:
-          // 1. follow user that tweets at us
+          request("http://expandurl.appspot.com/expand?url=" + shortened_url, function(error, response, body) {
+            if (!error) {
+              var json_body = JSON.parse( body )
+              
+              expanded_url = decodeURIComponent( json_body.end_url ) // like: http://example.com
 
-          request(shortened_url, function(error, response, body) {
-            if (!error && response.statusCode === 200) {
-              expanded_url = response.request.host
+              console.log("1: " + expanded_url)
 
-              domainr.info(expanded_url, function(responseFromDomainer) {
+              expanded_url = expanded_url.substr(expanded_url.indexOf('://')+3) // like: example.com
 
-                condole.log(responseFromDomainer)
+              console.log("2: " + expanded_url)
 
-                /*switch (responseFromDomainer.availability) {
+              domainr.info(expanded_url, function(responseFromDomainr) {
+         
+                switch (responseFromDomainr.availability) {
                   case "available":
-                    twit.updateStatus('@' + userToRespondTo + " " + expanded_url + " is available! You can register it here: " + responseFromDomainer.registrars.register_url + " <3", function(err, data) {
+                    twit.updateStatus('@' + userToRespondTo + " " + expanded_url + " is available! You can register it here: " + responseFromDomainr.register_url + " <3", function(err, data) {
                       if (err) { console.log(err) }
                     })
-                    break;
+                  break;
+
                   case "taken":
                     twit.updateStatus('@' + userToRespondTo + " " + expanded_url + " is taken =(", function(err, data) {
                       if (err) { console.log(err) }
                     })
                     break;
+
                   case "unknown":
                     twit.updateStatus('@' + userToRespondTo + " hmmm. Domai.nr says it's 'unknown'. Why don't you check on their site? http://domai.nr", function(err, data) {
                       if (err) { console.log(err) }
                     })
                     break;
-                }*/
-
+                }
 
 
               })
+
+
             }
           })
+
 
         }
       });
