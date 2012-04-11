7e20f2134fa5cb7345f38763fe67c1b7f3018eae
Connor Montgomery <c@cnnr.me>
2012-04-10 22:09:31 -0500

    working

--- app.js
+++ app.js
@@ -34,8 +34,6 @@ var twit = new Twitter({
 
         if (tweet.in_reply_to_screen_name === "checkthisdomain" && !tweet.retweeted) {
 
-          console.log(tweet)
-
           var shortened_url       = encodeURIComponent( tweet.text.split(' ')[1] )
             , userToRespondTo     = tweet.user.screen_name
             , reply_to_status_id  = tweet.id_str
@@ -43,7 +41,7 @@ var twit = new Twitter({
 
 
           request("http://expandurl.appspot.com/expand?url=" + shortened_url, function(error, response, body) {
-            if (!error) {
+            if (!error && !response.statusCode === 500) {
               var json_body = JSON.parse( body )
               
               expanded_url = decodeURIComponent( json_body.end_url ) // like: http://example.com
@@ -52,30 +50,42 @@ var twit = new Twitter({
 
               domainr.info(expanded_url, function(responseFromDomainr) {
          
-                switch (responseFromDomainr.availability) {
-                  case "available":
-                    twit.updateStatus('@' + userToRespondTo + " " + expanded_url + " is available! You can register it here: " + responseFromDomainr.register_url + " <3", {in_reply_to_status_id: reply_to_status_id}, function(err, data) {
-                      if (err) { console.log(err) }
-                    })
-                  break;
+                if (!responseFromDomainr.statusCode === 500) {
 
-                  case "taken":
-                    twit.updateStatus('@' + userToRespondTo + " " + expanded_url + " is taken =(", function(err, data) {
-                      if (err) { console.log(err) }
-                    })
-                    break;
+                    switch (responseFromDomainr.availability) {
+                      case "available":
+                        twit.updateStatus('@' + userToRespondTo + " " + expanded_url + " is available! You can register it here: " + responseFromDomainr.register_url + " <3", {in_reply_to_status_id: reply_to_status_id}, function(err, data) {
+                          if (err) { console.log(err) }
+                        })
+                      break;
 
-                  case "unknown":
-                    twit.updateStatus('@' + userToRespondTo + " hmmm. Domai.nr says it's 'unknown'. Why don't you check on their site? http://domai.nr", function(err, data) {
-                      if (err) { console.log(err) }
-                    })
-                    break;
+                      case "taken":
+                        twit.updateStatus('@' + userToRespondTo + " " + expanded_url + " is taken =(", function(err, data) {
+                          if (err) { console.log(err) }
+                        })
+                        break;
+
+                      case "unknown":
+                        twit.updateStatus('@' + userToRespondTo + " hmmm. Domai.nr says it's 'unknown'. Why don't you check on their site? http://domai.nr", function(err, data) {
+                          if (err) { console.log(err) }
+                        })
+                        break;
+                    }
+                } else {
+                  twit.updateStatus('@' + userToRespondTo + " " + expanded_url + " is an invalid search. Make sure it's a valid domain, or use http://domai.nr. Thx!", {in_reply_to_status_id: reply_to_status_id}, function(err, data) {
+                          if (err) { console.log(err) }
+                        }
+                    )
                 }
-
 
               })
 
 
+            } else {
+              twit.updateStatus('@' + userToRespondTo + " " + shortened_url + " is an invalid search. Make sure it's a valid domain, or use http://domai.nr. Thx!", {in_reply_to_status_id: reply_to_status_id}, function(err, data) {
+                  if (err) { console.log(err) }
+                }
+              )
             }
           })
 
